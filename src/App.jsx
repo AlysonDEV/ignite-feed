32851d91369c81f76bcc89fa3fa60c6560e23898
Alyson Ronnan Martins <devalyson@gmail.com>
2022-07-06 18:48:58 -0300

    estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { Post } from "./Post"
+import { Header } from "./components/Header"
 
-function App() {
+export function App() {
 
   return (
-    <div>
-      <Post 
-        autor="Alyson Ronnan" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi odio illo aliquam officia reiciendis eligendi laudantium laboriosam aut ipsa sunt, delectus culpa non repellat cumque ad eaque similique? Veniam, neque?"
-      />
-      <Post 
-        autor="Alyson Ronnan"
-        content="Valeu mesmo"
-      />
-      <Post />
-      <Post />
-      <Post />
-    </div>
+    <Header />
   )
 }
 
-export default App
