db18ca80c8dda4979a3f8b2b9e3c41aa71e0a9a1
Alyson Ronnan Martins <devalyson@gmail.com>
2022-08-03 19:16:24 -0300

    Os motores do React - Romovendo comentários (imutabilidade)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,8 +36,13 @@ export function Post({author, publishedAt, content}){
     setNewCommentText(event.target.value)
   }
 
-  function deleteComment(comment){
-    console.log(`Deletar o comentário: ${comment}`)
+  function deleteComment(commentToDelete){
+    const commentsWithoutDeleteOne = comments.filter(comment => {
+      return  comment !== commentToDelete
+    })
+    setComments([
+      commentsWithoutDeleteOne
+    ])
   }
 
   return (
